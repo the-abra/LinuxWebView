9c87c3d04e3af6b8ef638f7ce27720928a03e403
the-abra <egekilic@proton.me>
2025-02-01 00:57:10 +0300

    ENCH: webkit2gtk-4.0 lib sharing added, notify.c added, Lib Checkup added.

--- main.sh
+++ main.sh
@@ -14,13 +14,13 @@ if ! command -v gcc &>/dev/null; then
     
     # Update package list and install required dependencies
 
-    if ! ( apt update && apt install -y gcc g++ make cmake file libwebkit2gtk-4.0-dev libgtk-3-dev build-essential libfuse2 wget gstreamer1.0-libav gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly ); then
+    if  apt update &> /dev/null && apt install -y gcc g++ make cmake file libwebkit2gtk-4.0-dev libgtk-3-dev libglib2.0-bin build-essential libfuse2 wget gstreamer1.0-libav gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly ; then
         log.error "Failed to install dependencies. Exiting..."
         log.sub "try to install manualy."
         [[ -f ./module.pht ]] && log.sub "Access to shell : pht run LinuxWebView -c bash"
         exit 1
     else
-        pacman -Sy --noconfirm gcc file webkit2gtk gtk3 base-devel fuse2 cmake wget || {
+        pacman -Sy --noconfirm gcc file webkit2gtk gtk3 glib2 base-devel fuse2 cmake wget || {
         log.error "Failed to install dependencies. Exiting..."
         log.sub "try to install manualy."
         [[ -f ./module.pht ]] && log.sub "Access to shell : pht run LinuxWebView -c bash"
@@ -44,29 +44,44 @@ fi
 # Compile the vebviewer
 log.info "Compiling the application..."
 
+
+# webviewer.c
 gcc webviewer.c -o webview-app.AppDir/usr/bin/webviewer \
     $(pkg-config --cflags --libs  webkit2gtk-4.0 gtk+-3.0) \
-    -static-libgcc -static-libstdc++ &> /home/gccbuild.log || {
+    -static-libgcc -static-libstdc++ &> /home/webviewer.log || {
         log.error "Compilation failed."
-        log.sub "LOG: /home/gccbuild.log"
-        [[ $1 =~ (workflow|CI|CD) ]] && cat /home/gccbuild.log
+        log.sub "LOG: /home/webviewer.log"
+        [[ $1 =~ (workflow|CI|CD) ]] && cat /home/webviewer.log
         exit 1
     }
 
 log.done "GCC build completed successfully. (webviewer)"
 log.sub "SAVED -> webview-app.AppDir/usr/bin/webviewer"
 
+# localviewer.c
 gcc localviewer.c -o webview-app.AppDir/usr/bin/localviewer \
     $(pkg-config --cflags --libs  webkit2gtk-4.0 gtk+-3.0) \
-    -static-libgcc -static-libstdc++ &> /home/gccbuild.log || {
+    -static-libgcc -static-libstdc++ &> /home/localviewer.log || {
         log.error "Compilation failed."
-        log.sub "LOG: /home/gccbuild.log"
-        [[ $1 =~ (workflow|CI|CD) ]] && cat /home/gccbuild.log
+        log.sub "LOG: /home/localviewer.log"
+        [[ $1 =~ (workflow|CI|CD) ]] && cat /home/localviewer.log
         exit 1
     }
 
 log.done "GCC build completed successfully. (localviewer)"
 log.sub "SAVED -> webview-app.AppDir/usr/bin/localviewer"
+
+# notify.c
+gcc notify.c -o webview-app.AppDir/usr/bin/notify \
+    `pkg-config --cflags --libs gtk+-3.0` &> /home/notifybuild.log || {
+        log.error "Compilation failed."
+        log.sub "LOG: /home/notifybuild.log"
+        [[ $1 =~ (workflow|CI|CD) ]] && cat /home/notifybuild.log
+        exit 1
+    }
+
+log.done "GCC build completed successfully. (notify)"
+log.sub "SAVED -> webview-app.AppDir/usr/bin/notify"
 
 # Generate the AppImage and try to run
 ! [[ -d build ]] && mkdir build
